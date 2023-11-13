f90df33ad788e280cbf688d66969236b655348fe
WyattBensman <wyattbensman5@gmail.com>
2023-11-13 14:39:53 -0500

    Completed typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,57 @@
+const typeDefs = `
+type User {
+    _id: ID!
+    fName: String!
+    lName: String!
+    email: String!
+    password: String!
+    lists: [List]
+    savedStores: [Store]
+  }
+  
+  type List {
+    _id: ID!
+    userId: ID!
+    title: String!
+    listItems: [Item]
+  }
+  
+  type Item {
+    _id: ID!
+    itemName: String!
+    itemPrice: Float!
+    itemSize: String!
+    itemUrl: String!
+  }
+  
+  type Store {
+    _id: ID!
+    name: String!
+    url: String!
+    image: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user(userId: ID!): User
+    list(listId: ID!): List
+    stores: [Store]
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    createUser(fName: String!, lName: String!, email: String!, password: String!): Auth
+    createList(userId: ID!, title: String!): List
+    deleteList(listId: ID!): List
+    createItem(listId: ID!, itemName: String!, itemPrice: Float!, itemSize: String, itemUrl: String!): Item
+    deleteItem(listId: ID!, itemId: ID!): Item
+    saveStore(userId: ID!, storeId: ID!): User
+    unsaveStore(userId: ID!, storeId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
