5d0f504e51422bc2706bc5a024cb45dc251e8eeb
WyattBensman <wyattbensman5@gmail.com>
2024-01-11 09:55:20 -0500

    -_-

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,4 +5,8 @@ require("dotenv").config();
 // DB Name goes at the end ------->
 connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/merryWishlistDB");
 
+connection.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
 module.exports = connection;
