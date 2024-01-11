f4761cef8b8fda38a3aaa8a0a8eca88015296499
WyattBensman <wyattbensman5@gmail.com>
2024-01-11 09:32:31 -0500

    Help

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-/* const bcrypt = require("bcrypt"); */
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   fName: {
@@ -45,19 +45,19 @@ const userSchema = new Schema({
 });
 
 // Hashes Password
-/* userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function (next) {
   if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
 
   next();
-}); */
+});
 
 // Checks if Password is Correct
-/* userSchema.methods.isCorrectPassword = async function (password) {
+userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
-}; */
+};
 
 // Create the User model
 const User = model("User", userSchema);
